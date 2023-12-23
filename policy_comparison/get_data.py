bfa65a76c9606fcea0c3b1dbfba34c3a3a648815
mahaswec <mchakraborti@ucdavis.edu>
2023-12-23 14:16:25 +0530

    generate rule cache

--- policy_comparison/get_data.py
+++ policy_comparison/get_data.py
@@ -8,9 +8,9 @@ f = open('/content/IG-SRL/policy_comparison/data/reddit_rules_top_100.json')
 data = json.load(f)
 
 community1 = data[drop_down.value]['rules']
-db1 = pd.DataFrame(community1, columns=["raw institutional statement"])
+db1 = pd.DataFrame(community1, columns=["Raw Institutional Statement"])
 community2 = data[dependent_drop_down.value]['rules']
-db2 = pd.DataFrame(community2, columns=["raw institutional statement"])
+db2 = pd.DataFrame(community2, columns=["Raw Institutional Statement"])
 
 db1.to_csv("/content/db1.csv",index=False)
 db2.to_csv("/content/db2.csv",index=False)
