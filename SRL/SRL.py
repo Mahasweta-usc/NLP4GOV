baba2e665b710e69cf9fde02053c04ffaf94076a
mahaswec <mchakraborti@ucdavis.edu>
2023-12-24 19:07:52 -0800

    update rule

--- SRL/SRL.py
+++ SRL/SRL.py
@@ -160,14 +160,15 @@ class SRL:
 
     if arg == 'object_inf':
         keys = sorted(list(set(main_arguments) & set(x.keys())))
-        if 'ARG0' in x:
-            keys.remove('ARG0')
-            if keys: return ", ".join(x[keys[0]])
+        if 'ARG0' in x or (self.detect_sub(text) and 'ARG1' in x):
+            try:
+                print(keys, keys[1])
+                return ", ".join(x[keys[1]])
+            except: return ""
 
-        if self.detect_sub(text) and 'ARG1' in x:
-            keys.remove('ARG1')
-
-        if keys : return ", ".join(x[keys[0]])
+        if keys:
+            print(keys, keys[0])
+            return ", ".join(x[keys[0]])
         else: return ""
 
     if arg == 'aim_inf': return " ".join(x['V'])
