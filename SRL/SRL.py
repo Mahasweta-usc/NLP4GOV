6dd19660a87730882ae9d0b7609673faab7f196b
mahaswec <mchakraborti@ucdavis.edu>
2023-12-25 22:49:46 +0530

    updated gen_graph

--- SRL/SRL.py
+++ SRL/SRL.py
@@ -287,8 +287,6 @@ class SRL:
             data[col_name] = data[col_name].apply(lambda x: re.sub("[\(\[].*?[\)\]]", "", x))
 
 
-        data.to_csv(out_path.replace(".csv","_int.csv"), index=False)
-
         data['attribute'] = data.apply( lambda x : "<skipped>" if x.attribute and (x.attribute not in x['raw institutional statement']) else x.attribute, axis=1)
         data['object'] = data.apply( lambda x : "<skipped>" if x.object and (x.object not in x['raw institutional statement']) else x.object, axis=1)
 
