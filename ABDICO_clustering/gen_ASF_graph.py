83abcc6573cc4a25799d9c20ac6e2e6979bda1a9
mahaswec <mchakraborti@ucdavis.edu>
2023-12-26 18:27:14 +0530

    fixed edge style

--- ABDICO_clustering/gen_ASF_graph.py
+++ ABDICO_clustering/gen_ASF_graph.py
@@ -85,9 +85,9 @@ G = nx.MultiDiGraph()
 #     G.add_edge(row.Attribute_group, row.Object_group, color=row.Deontic)
 
 for _, row in result.iterrows():
-    if G.has_edge(row.Attribute_group, row.Object_group, color=row.Deontic):
+    data = G.get_edge_data(row.Attribute_group, row.Object_group, color=row.Deontic, default = None)
+    if data:
         # we added this one before, just increase the weight by one
-        data = G.get_edge_data(row.Attribute_group, row.Object_group, color=row.Deontic)
         G.add_edge(row.Attribute_group, row.Object_group, color=row.Deontic, weight=data['weight'] + 1)
         G.remove_edge(row.Attribute_group, row.Object_group, color=row.Deontic, weight=data['weight'])
 
