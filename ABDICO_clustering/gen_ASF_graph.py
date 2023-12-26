54554b0c968fd2b5a4beba4bcfd54d51d50946d8
mahaswec <mchakraborti@ucdavis.edu>
2023-12-26 10:52:50 +0530

    removing non span

--- ABDICO_clustering/gen_ASF_graph.py
+++ ABDICO_clustering/gen_ASF_graph.py
@@ -28,7 +28,7 @@ entries = result['Attribute'].tolist()
 entries.extend(result['Object'].tolist())
 hdbscan_model = HDBSCAN(metric='euclidean', cluster_selection_method='eom',
                         prediction_data=True)
-topic_model = BERTopic(top_n_words=3, hdbscan_model=hdbscan_model, nr_topics='auto')
+topic_model = BERTopic(top_n_words=10, hdbscan_model=hdbscan_model)
 topic_model.hdbscan_model.gen_min_span_tree = True
 topic_model.umap_model.random_state = 0  ##set seed to enable reproduction of clustering
 
