8e66b88baa5490a1d5f232b5bad22b1894138ef6
mahaswec <mchakraborti@ucdavis.edu>
2023-12-25 23:42:50 -0800

    fixed graph viz

--- ABDICO_clustering/gen_ASF_graph.py
+++ ABDICO_clustering/gen_ASF_graph.py
@@ -55,7 +55,7 @@ result['Deontic'] = result['Deontic'].apply(lambda x : x if x in deontic_map els
 
 entries = result['Attribute'].tolist()
 entries.extend(result['Object'].tolist())
-hdbscan_model = HDBSCAN(metric='euclidean', cluster_selection_method='eom',min_cluster_size=10, min_samples=2,
+hdbscan_model = HDBSCAN(metric='euclidean', cluster_selection_method='eom',min_cluster_size=10, min_samples=10,
                         prediction_data=True)
 topic_model = BERTopic(top_n_words=3, hdbscan_model=hdbscan_model, nr_topics='auto')
 topic_model.hdbscan_model.gen_min_span_tree = True
