8e5bb18525fb49244df1cc4bcb78a9462aa3f3ff
mahaswec <mchakraborti@ucdavis.edu>
2024-01-24 21:20:26 -0700

    fixed edge weights

--- ABDICO_clustering/gen_ASF_graph.py
+++ ABDICO_clustering/gen_ASF_graph.py
@@ -118,7 +118,7 @@ for idx, row in result.iterrows():
     except Exception as exp:
         G.add_edge(row.Attribute_group, row.Object_group, weight = 1, color=row.Deontic, key=row.Deontic)
 
-pos = nx.spring_layout(G, k=0.5)
+pos = nx.spring_layout(G, k=0.1)
 # ax = plt.gca()
 fig, axes = plt.subplots(2, 2, figsize=(30, 50))
 axes = axes.flatten()
