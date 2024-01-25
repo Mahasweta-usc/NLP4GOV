1def98b6c8d1e5c6de7688f55d6dacc8cad8874c
mahaswec <mchakraborti@ucdavis.edu>
2024-01-24 19:45:13 -0700

    fixed edge weights

--- ABDICO_clustering/gen_ASF_graph.py
+++ ABDICO_clustering/gen_ASF_graph.py
@@ -147,18 +147,18 @@ for idx, shade in enumerate((SNR_map.keys())):
 
     # print(shade, "in_degree: ", in_track)
     # print("out_degree", out_track)
-    
-    axes[idx].set_title(SNR_map[shade], fontsize=32, fontweight='heavy')
-    # #draw node edges
-    # nodes = nx.draw_networkx_nodes(G, pos, node_color='lemonchiffon', nodelist=set(nodes), node_size=40000, )
-    # nx.draw_networkx_labels(G, pos, font_size=25, alpha=1)
-    # nodes.set_edgecolor('r')
 
-    pic = nx.draw_networkx(G, pos, node_color='lemonchiffon', nodelist=set(nodes), font_size=25, edgelist=edges,
-                     edge_color=shade, width=weights,
-                     node_size=40000, alpha=1, with_labels=True, font_weight='bold',
-                     connectionstyle=f"arc3,rad=-0.5",
-                     arrowstyle=f"-|>,head_length=1.5,head_width=1.2", ax=axes[idx])  #
+    axes[idx].set_title(SNR_map[shade], fontsize=32, fontweight='heavy')
+    #draw node edges
+    nodes = nx.draw_networkx_nodes(G, pos, node_color='lemonchiffon', nodelist=set(nodes), node_size=40000)
+    nx.draw_networkx_labels(G, pos, font_size=25, alpha=1 , font_weight='bold')
+    nodes.set_edgecolor('r')
+
+    # pic = nx.draw_networkx(G, pos, node_color='lemonchiffon', nodelist=set(nodes), font_size=25, edgelist=edges,
+    #                  edge_color=shade, width=weights,
+    #                  node_size=40000, alpha=1, with_labels=True, font_weight='bold',
+    #                  connectionstyle=f"arc3,rad=-0.5",
+    #                  arrowstyle=f"-|>,head_length=1.5,head_width=1.2", ax=axes[idx])  #
     # pic.set_edgecolor('r')
     # ax.axis('off')
 
